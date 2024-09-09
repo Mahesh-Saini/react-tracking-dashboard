baef2b9a1e4776dea7cc1f0e92a17f2ca19cb728
mahesh <maheshkumar@gmail.com>
2024-09-09 22:11:09 +0530

    bar chart and Pie chart added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,10 +3,15 @@ import { MdEmojiTransportation } from "react-icons/md";
 import {
   Area,
   AreaChart,
+  Bar,
+  BarChart,
   CartesianGrid,
   Legend,
   Line,
   LineChart,
+  Pie,
+  PieChart,
+  ResponsiveContainer,
   Tooltip,
   XAxis,
   YAxis,
@@ -111,9 +116,100 @@ const data = [
   },
 ];
 
+const data01 = [
+  {
+    name: "Group A",
+    value: 400,
+  },
+  {
+    name: "Group B",
+    value: 300,
+  },
+  {
+    name: "Group C",
+    value: 300,
+  },
+  {
+    name: "Group D",
+    value: 200,
+  },
+  {
+    name: "Group E",
+    value: 278,
+  },
+  {
+    name: "Group F",
+    value: 189,
+  },
+];
+const data02 = [
+  {
+    name: "Group A",
+    value: 2400,
+  },
+  {
+    name: "Group B",
+    value: 4567,
+  },
+  {
+    name: "Group C",
+    value: 1398,
+  },
+  {
+    name: "Group D",
+    value: 9800,
+  },
+  {
+    name: "Group E",
+    value: 3908,
+  },
+  {
+    name: "Group F",
+    value: 4800,
+  },
+];
+
+const data10 = [
+  {
+    name: "Page A",
+    uv: 4000,
+    pv: 2400,
+  },
+  {
+    name: "Page B",
+    uv: 3000,
+    pv: 1398,
+  },
+  {
+    name: "Page C",
+    uv: 2000,
+    pv: 9800,
+  },
+  {
+    name: "Page D",
+    uv: 2780,
+    pv: 3908,
+  },
+  {
+    name: "Page E",
+    uv: 1890,
+    pv: 4800,
+  },
+  {
+    name: "Page F",
+    uv: 2390,
+    pv: 3800,
+  },
+  {
+    name: "Page G",
+    uv: 3490,
+    pv: 4300,
+  },
+];
+
 const Home = () => {
   return (
-    <div className="container-fluid px-5 py-4">
+    <div className="container px-5 py-4">
       <div className="row d-flex align-items-center justify-content-center">
         <div className="col-3 p-2 d-flex">
           <div className="container bg-primary text-light py-2 rounded-pill">
@@ -175,17 +271,15 @@ const Home = () => {
         </div>
       </div>
 
-      <div className="row mt-4">
+      <div className="row mt-4 gap-4">
         <div
           className="col-12 bg-white rounded p-4"
           style={{ minHeight: "400px" }}
         >
-          <div className="container">
+          <ResponsiveContainer width="100%" height="100%">
             <LineChart
-              width={1000}
-              height={400}
               data={data}
-              margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+              margin={{ top: 5, right: 30, left: 0, bottom: 5 }}
             >
               <CartesianGrid strokeDasharray="3 3" />
               <XAxis dataKey="name" />
@@ -197,24 +291,53 @@ const Home = () => {
               <Line type="monotone" dataKey="dealerPoint" stroke="#82aa9d" />
               <Line type="monotone" dataKey="onTime" stroke="#82ca9d" />
             </LineChart>
-          </div>
+          </ResponsiveContainer>
         </div>
-        <div className="col-6 bg-primary" style={{ minHeight: "400px" }}>
-          Hi
+        <div
+          className="col-12 bg-white rounded p-4"
+          style={{ minHeight: "400px" }}
+        >
+          <ResponsiveContainer width="100%" height="100%">
+            <PieChart width={730} height={250}>
+              <Pie
+                data={data01}
+                dataKey="value"
+                nameKey="name"
+                cx="50%"
+                cy="50%"
+                outerRadius={50}
+                fill="#8884d8"
+              />
+              <Pie
+                data={data02}
+                dataKey="value"
+                nameKey="name"
+                cx="50%"
+                cy="50%"
+                innerRadius={60}
+                outerRadius={80}
+                fill="#82ca9d"
+                label
+              />
+            </PieChart>
+          </ResponsiveContainer>
         </div>
-        <div className="col-6 bg-danger" style={{ minHeight: "400px" }}>
-          Good
-        </div>
-        <div className="col-6 bg-secondary" style={{ minHeight: "400px" }}>
-          Good
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-12 bg-success" style={{ minHeight: "560px" }}>
-          hello
-        </div>
-        <div className="col-12 bg-primary" style={{ minHeight: "560px" }}>
-          Hi
+
+        <div
+          className="col-12 bg-white rounded p-4"
+          style={{ minHeight: "400px" }}
+        >
+          <ResponsiveContainer width="100%" height="100%">
+            <BarChart width={730} height={250} data={data10}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Bar dataKey="pv" fill="#8884d8" />
+              <Bar dataKey="uv" fill="#82ca9d" />
+            </BarChart>
+          </ResponsiveContainer>
         </div>
       </div>
     </div>
